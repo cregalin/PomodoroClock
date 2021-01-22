b7b9094c63366ea3d7371fe8b9c2b1d32f734009
Carol Regalin <caroline.regalin@gett.com.br>
2021-01-21 22:41:54 -0300

    feat(pomodoro): layout inicial e contador de segundos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import Async from "./examples/async";
+import { Card, Button, CardTitle, CardText } from 'reactstrap';
+import React, { useState, useRef, useEffect } from 'react';
 
 function App() {
-  return (
-    <div>
-      <p>
-        PROJETO POMODORO CLOCK
-      </p>
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
 
-      <div>
-        Alguns exemplos:
-        <ul>
-          <li>Async/Sync <Async /></li>
-        </ul>
-      </div>
+  const Stop = () => {
+  }
+
+  const Start = () => {
+  }
+
+  const Reset = () => {
+    setMinutes(25);
+    setSeconds(0);
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds === 0 ? 59 : seconds - 1);
+      console.log(seconds);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <div class="container">
+      <Card body outline color="secondary" className="text-center">
+        <CardTitle tag="h5">Pomodoro Timer</CardTitle>
+        <CardText>{minutes}:{seconds}</CardText>
+        <div className="mb-2">
+          <Button onClick={Stop} color="danger" size="lg">
+           Stop
+          </Button>{' '}
+          <Button onClick={Start} color="success" size="lg">
+            Start
+          </Button> {' '}
+          <Button onClick={Reset} color="secondary" size="lg">
+            Reset
+          </Button>
+        </div>
+      </Card>
     </div>
   );
 }
